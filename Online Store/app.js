c1e8a3dada1b486ad88760b4ffc5c8ff366de31d
Navrose <navrosesinghjohal@gmail.com>
2024-08-07 10:03:07 -0400

    Added subtotal logic

--- Online Store/app.js
+++ Online Store/app.js
@@ -58,6 +58,7 @@ app.post('/submit', [
         res.render('receipt', { items })
     } else {
         const items = Object.keys(req.body).map(key => (req.body)[key]);
+        items[9] = `Subtotal: ${(Number(items[6]) * 5 + Number(items[7]) * 2 + Number(items[8]) * 3)}`
         items[6] = "No.of hashBrowns " + items[6];
         items[7] = "No.of Coffee cups " + items[7];
         items[8] = "No.of Bagels " + items[8];
