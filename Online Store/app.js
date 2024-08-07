6ba3673122d8756717c4c3cc0acb43ba9c39f9dc
Navrose <navrosesinghjohal@gmail.com>
2024-08-07 00:03:15 -0400

    Added validation code in such  a way that receipt ejs form doesn't have to be modified.

--- Online Store/app.js
+++ Online Store/app.js
@@ -1,6 +1,8 @@
 "use strict";
 const express = require('express'),
     validator = require('express-validator'),
+    check = validator.check,
+    validationResult = validator.validationResult,
     app = express();
 
 // env set methods 
@@ -20,19 +22,25 @@ app.get('/allorders', (req, res) => {
 
 
 // validate and process form submission;
-app.post('/submit', (req, res) => {
+app.post('/submit', [
+    check('name').notEmpty().withMessage("name is required"),
+    check('email').isEmail().withMessage('Please enter a valid email.')
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        let errArr = errors.array();
+        const items = [];
+        for (let i = 0; i < errArr.length; i++) {
+            items.push(errArr[i].msg)
+        }
+        res.render('receipt', { items })
+    }
     const items = ["name", 345825205, "nav@gmail"]
     res.render("receipt", { items })
 })
 
 
 
-
-
-
-
-
-
 app.listen(app.get("port"), () => {
     console.log(`App running on http://localhost:${app.get('port')}`)
-})
+}) 
